5fad9cfa77ddd79cfe79d6b350c849a8b6eea8df
PRANAV RAIKAR <pranavraikar01@gmail.com>
2023-03-03 19:41:14 +0530

    added new codes

--- chalenges/urls.py
+++ chalenges/urls.py
@@ -4,5 +4,16 @@ from django.urls import path
 
 
 urlpatterns=[
-    path("january", views.index)    #i.e if i request /january then execute index function
+    path("january", views.index1),    #i.e if i request /january then execute index function
+    path("february",views.index2),
+    path("march",views.index3),
+    path("april",views.index4),
+    path("may",views.index5),
+    path("june",views.index6),
+    path("july",views.index7),
+    path("august",views.index8),
+    path("september",views.index9),
+    path("october",views.index10),
+    path("november",views.index11),
+    path("december",views.index12),
 ]
--- chalenges/views.py
+++ chalenges/views.py
@@ -2,6 +2,28 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
-def index(request):
-    return HttpResponse("This works!")      #html pages can also be returned as resposes 
+d="december"
+def index1(request):
+    return HttpResponse("This is the ,month of january!")      #html pages can also be returned as resposes 
+def index2(request):
+    return HttpResponse("This is the month of february!") 
+def index3(request):
+    return HttpResponse("This is the month of march!") 
+def index4(request):
+    return HttpResponse("This is the month of april!") 
+def index5(request):
+    return HttpResponse("This is the month of may!") 
+def index6(request):
+    return HttpResponse("This is the month of june!") 
+def index7(request):
+    return HttpResponse("This is the month of july!") 
+def index8(request):
+    return HttpResponse("This is the month of august!") 
+def index9(request):
+    return HttpResponse("This is the month of september!") 
+def index10(request):
+    return HttpResponse("This is the month of october!") 
+def index11(request):
+    return HttpResponse("This is the month of november!") 
+def index12(request):
+    return HttpResponse("This is the month of  "+ d +" !")  #just declared a variable and used it 
